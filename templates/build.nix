197a8c2f6aefeb852ebf1670094c1269e36e016a
jaredponn <>
2025-02-10 21:13:34 -0700

    Fix README typos, and minor syntactic changes to `templates/build.nix`

--- templates/build.nix
+++ templates/build.nix
@@ -1,9 +1,11 @@
 # `./templates` includes files for creating purus projects from a template (using nix templates)
 { ... }: {
 
-  flake.templates.default = {
-    path = ./default;
-    description =
-      "Example flake for developing with Purus using purus-pkg to manage dependencies";
+  flake.templates = {
+    default = {
+      path = ./default;
+      description =
+        "Example flake for developing with Purus using purus-pkg to manage dependencies";
+    };
   };
 }
