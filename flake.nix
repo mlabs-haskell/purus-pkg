034eda92bc57461861b5b51146c2828f441023a7
jaredponn <>
2025-02-10 20:45:06 -0700

    Remove useless comments + update purus dependency

--- flake.nix
+++ flake.nix
@@ -14,17 +14,7 @@
     haskell-nix.url = "github:input-output-hk/haskell.nix";
 
     # purus
-    purus.url =
-      "github:mlabs-haskell/purus?ref=jaredponn/create-purus-flake-output";
-
-    # # Crypto overlays necessary for Plutus
-    # iohk-nix.url = "github:input-output-hk/iohk-nix";
-    # iohk-nix.inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
-
-    # # Cardano Haskell packages
-    # cardano-haskell-packages.url =
-    #   "github:IntersectMBO/cardano-haskell-packages?ref=repo";
-    # cardano-haskell-packages.flake = false;
+    purus.url = "github:mlabs-haskell/purus";
   };
 
   outputs = inputs@{ flake-parts, ... }:
